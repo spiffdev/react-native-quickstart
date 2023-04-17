baf3331b42ebaca5a227a4f004be69962f5dc35c
Joshua Jones <joshuajones2010@gmail.com>
2023-04-17 10:28:27 +1000

    Re-added core initialisation. Workaround for auth issue.

--- App.tsx
+++ App.tsx
@@ -3,14 +3,42 @@
  * @format
  */
 
-import React from "react";
+import { SpiffCommerceClient, WorkflowExperience } from "@spiffcommerce/core";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 /**
  * This is the main wrapper component for the App editor.
  * See app in src/index.tsx for usage.
  */
-const App: React.FunctionComponent<{}> = () => {
+const App: React.FunctionComponent<{
+    /**
+     * The workflow to be used.
+     */
+    workflowId: string;
+    /**
+     * The integration product associated to the workflow being run.
+     */
+    integrationProductId: string;
+}> = ({ workflowId, integrationProductId }) => {
+    const [workflowExperience, setWorkflowExperience] = useState<WorkflowExperience | undefined>(undefined);
+    // This effect handles initialize of the workflow experience when the user first arrives at the page. Loading
+    // saved designs will be handled within App separately.
+    useEffect(() => {
+        const init = async () => {
+            // TODO: Core currently does not provide a way to supply a custom storage provider. This should be updated once that is available.
+            const client = new SpiffCommerceClient({});
+            await client.initFromIntegrationProduct(integrationProductId);
+            const experience = await client.getWorkflowExperience(workflowId, undefined, (workflow) => {
+                // TODO: implement a 3D preview service.
+            });
+            // experience.getWorkflowManager().getPreviewService().registerView(canvasRef.current);
+            setWorkflowExperience(experience);
+        };
+        init().then(() => console.log("Workflow Experience Initialized"));
+        // We only want this to run when the parameters passed in change. The workflow experience
+        // changing internally due to saved designs etc.. Should not trigger this.
+    }, [integrationProductId, workflowId]);
     return (
         <View
             style={{
